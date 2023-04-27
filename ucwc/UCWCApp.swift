d2433257f4033ae84fa0697915c4bfe3c4ea68c1
rivaldofez <rivaldofez@gmail.com>
2023-04-27 17:50:52 +0700

    init project

---
+++ ucwc/UCWCApp.swift
@@ -0,0 +1,17 @@
+//
+//  ucwcApp.swift
+//  ucwc
+//
+//  Created by Rivaldo Fernandes on 27/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ucwcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
